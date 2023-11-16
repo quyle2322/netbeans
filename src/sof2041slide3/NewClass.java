45ef90b76ca7d7ca7ae276447ca2da38a0bf395c
Truong <johndoe@gmail.com>
2023-11-16 08:25:12 +0700

    First commit

--- src/sof2041slide3/NewClass.java
+++ src/sof2041slide3/NewClass.java
@@ -4,14 +4,65 @@
  */
 package sof2041slide3;
 
-import javax.xml.transform.Source;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-/**
- *
- * @author Truong
- */
-public class NewClass {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+
+public class NewClass extends JFrame{
+    JLabel pic;
+    Timer tm;
+    int x = 0;
+    //Images Path In Array
+    String[] list = {
+                      "C:\\Users\\Truong\\Downloads\\Image\\1.jpg",//0
+                      "C:\\Users\\Truong\\Downloads\\Image\\2.jpg",
+                       "C:\\Users\\Truong\\Downloads\\Image\\3.jpg",
+            "C:\\Users\\Truong\\Downloads\\Image\\4.jpg",//0
+                      "C:\\Users\\Truong\\Downloads\\Image\\5.jpg",
+                       "C:\\Users\\Truong\\Downloads\\Image\\6.jpg"
+                    };
+    
+    public NewClass(){
+        super("Java SlideShow");
+        pic = new JLabel();
+        pic.setBounds(40, 30, 700, 300);
+
+        //Call The Function SetImageSize
+        SetImageSize(5);
+               //set a timer
+        tm = new Timer(1500,new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SetImageSize(x);
+                x += 1;
+                if(x >= list.length )
+                    x = 0; 
+            }
+        });
+        add(pic);
+        tm.start();
+        setLayout(null);
+        setSize(800, 400);
+        getContentPane().setBackground(Color.decode("#bdb67b"));
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
+    //create a function to resize the image 
+    public void SetImageSize(int i){
+        ImageIcon icon = new ImageIcon(list[i]);
+        Image img = icon.getImage();
+        Image newImg = img.getScaledInstance(pic.getWidth(), pic.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon newImc = new ImageIcon(newImg);
+        pic.setIcon(newImc);
+    }
+
+public static void main(String[] args){ 
+      
+    new NewClass();
+}
 }
---
+++ src/sof2041slide3/NewClass1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sof2041slide3;
+
+/**
+ *
+ * @author Truong
+ */
+public class NewClass1 {
+    
+}
